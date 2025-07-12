37ee0c3e16147a0815b3ebb303d7102da02b4ae1
Jaimendo99 <jaime.mendoza@udla.edu.ec>
2025-07-12 01:41:13 -0500

    feat: update webhook model to support more fields

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 import uvicorn
 
 class OdooWebhook(BaseModel):
     _action: str
     _id: int
     _model: str
+    amount_paid: Optional[float] = None
+    amount_residual: Optional[float] = None
+    amount_total_words: Optional[str] = None
+    display_name: Optional[str] = None
+    highest_name: Optional[str] = None
+    id: int
+    invoice_filter_type_domain: Optional[bool] = None
+    invoice_line_ids: Optional[List[int]] = None
+    is_lab_invoice: Optional[bool] = None
+    move_type: Optional[str] = None
+    type_name: Optional[str] = None
 
 app = FastAPI()
 
